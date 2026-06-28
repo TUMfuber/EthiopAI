3ce324221e77f98a75424c45e19b1661b1133056
Francesco Uber (Laptop) <francesco.uber@tum.de>
2026-06-28 13:27:31 +0200

    Remove heatmap visualisation

--- modules/map-service/src/components/EthiopiaMap.tsx
+++ modules/map-service/src/components/EthiopiaMap.tsx
@@ -792,7 +792,6 @@ export default function EthiopiaMap({
           <RawPointsLayer leaves={flatLeaves} />
         )}
 
-        <PriorityHeatmap visible={priorityActive} />
         <DetailLayer visible={priorityActive} />
         <ActionPointMarkers points={actionPoints} />
         <DropMarker active={dropMode} onDrop={(lat, lng) => { setDroppedPin({ lat, lng }); setDropMode(false); }} />
